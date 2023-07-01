121d2a3e2aec88538fdd7e42ea8dfef2c6306345
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-07-01 13:16:43 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 document.querySelector('.navbar .menu-btn').addEventListener('click', () => {
     document.querySelector('.navbar .links ul').classList.toggle("active");
-    document.querySelector('.navbar').classList.toggle("active")
     document.querySelectorAll('.menu-btn i').forEach((e) => {
         e.classList.toggle("active")
     })
